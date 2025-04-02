4f011a3b09957389ccdb695692bc4e286a480b5a
abdullah <khanmohaiymin@gmail.com>
2025-04-02 14:40:43 +0500

    feat: :art: Added some nice mouse shadow

--- js/main.js
+++ js/main.js
@@ -347,3 +347,12 @@
     ssMoveTo();
   })();
 })(document.documentElement);
+
+document.addEventListener("mousemove", (e) => {
+  gsap.to(".cursor", {
+    x: e.clientX,
+    y: e.clientY,
+    duration: 0.1,
+    ease: "power2.out",
+  });
+});
